0ffe7fca5c51228af1ea92346bf905abdeb9013b
waggertron <weylinwagnon@gmail.com>
2021-05-25 07:51:39 -0700

    update info

--- tvdb_api_v4/setup.py
+++ tvdb_api_v4/setup.py
@@ -10,17 +10,18 @@ setup(
     name="tvdb-api-v4",
     version=VERSION,
     author="Weylin Wagnon",
-    author_email="<wwagnon@whipmedia.com>",
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    author_email="<support@thetvdb.com>",
+    url='https://github.com/thetvdb/tvdb-v4-python',
+    description="tvdb-api-v4 utility package",
+    long_description="official python client for the tvdb api v4",
     packages=find_packages(),
     install_requires=[],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
 
-    keywords=['python', 'first package'],
+    keywords=['python', 'tvdb'],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Intended Audience :: Api users",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Operating System :: MacOS :: MacOS X",
