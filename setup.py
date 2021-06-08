22198f0d626ce3de0bfd1bceaefc8ab5a95ff133
waggertron <weylinwagnon@gmail.com>
2021-06-07 17:20:10 -0700

    cosolidate

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
-VERSION = '1.0.2'
+VERSION = '1.0.7'
 DESCRIPTION = 'Official tvdb api v4 package'
 LONG_DESCRIPTION = 'Official python package for using the tvdb v4 api'
 
 # Setting up
 setup(
-    name="tvdb_official",
+    name="tvdb_v4_official",
     version=VERSION,
     author="Weylin Wagnon",
     author_email="<support@thetvdb.com>",
@@ -15,14 +15,14 @@ setup(
     long_description="official python client for the tvdb api v4",
     packages=find_packages(),
     install_requires=[],
-
+    py_modules=['tvdb_v4_official'],
     keywords=['python', 'tvdb'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    package_dir={'tvdb_v4_official': 'tvdb_v4_official'}
 )
