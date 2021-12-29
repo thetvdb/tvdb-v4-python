cb53e52d9b17c72b3cfc9afe54c2b375f237780d
Wayne Davison <wayne@opencoder.net>
2021-12-29 11:04:03 -0800

    Remove duplicate function.

--- tvdb_v4_official.py
+++ tvdb_v4_official.py
@@ -227,11 +227,6 @@ class TVDB:
         url = self.url.construct('episodes', id, 'extended', meta=meta)
         return self.request.make_request(url)
     
-    def get_episodes_translation(self, id: int, lang: str) -> dict:
-        """Returns an episode translation dictionary"""
-        url = self.url.construct('episodes',id,'translations', lang)
-        return self.request.make_request(url)
-
     def get_episode_translation(self, id: int, lang: str, meta=None) -> dict:
         """Returns an episode translation dictionary"""
         url = self.url.construct('episodes', id, 'translations', lang, meta=meta)
