d9b62305e6078f508327f0652a9cd512324391bc
Maximliano Fernandez Wortman <maxifwortman@gmail.com>
2022-08-14 21:05:33 -0300

    version 1.0.12

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = '1.0.11'
+VERSION = '1.0.12'
 DESCRIPTION = 'Official tvdb api v4 package'
 LONG_DESCRIPTION = 'Official python package for using the tvdb v4 api'
 
